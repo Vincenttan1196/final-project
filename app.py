836c6f9b222638c9b50de8eacd6f69a01069476a
Vincent Tan <181196T@mymail.nyp.edu.sg>
2018-12-09 22:11:38 +0800

    Table

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
 def index():
     return "This is the homepage"
 
-@app.route("/test")
-def test():
-    return "<h1> Hello World this is vincent </h1>"
+@app.route("/summary/<familyid>")
+def summary(familyid):
+    return render_template("summary.html", familyid = familyid)
 
 if __name__ == "__main__":
     app.run()
