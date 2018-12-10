32a30f1e580ab585c704e4efd27ae293f431f8e4
test <181530J@mymail.nyp.edu.sg>
2018-12-10 14:54:01 +0800

    WHATS GOING ON

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template
+from persistence import *
+
+
+
 
 app = Flask(__name__)
 
@@ -12,3 +16,51 @@ def summary(familyid):
 
 if __name__ == "__main__":
     app.run()
+
+
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if session['id'] is None:
+            return redirect(url_for('login'))
+        return view(**kwargs)
+    return wrapped_view
+
+@app.route('/login',  methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        else:
+            user = get_user(username, password)
+            if user is None:
+                error = 'Wrong username and password'
+            else:
+                session['id'] = user.get_id()
+                session['user_name'] = user.get_username()
+                return redirect(url_for('index'))
+        flash(error)
+    return render_template('login.html')
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        else:
+            create_user(username, password)
+            return redirect(url_for('login'))
+        flash(error)
+    return render_template('register.html')
---
+++ persistence.py
@@ -0,0 +1,67 @@
+import shelve
+import uuid
+from datetime import date
+# today = str(date.today())
+
+class User:
+    def __init__(self, id):
+        self.__id = id
+        self.__username = ''
+        self.__password = ''
+
+    def get_id(self):
+        return self.__id
+
+    def set_username(self, username):
+        self.__username = username
+
+    def set_password(self, password):
+        self.__password = password
+
+    def get_username(self):
+        return self.__username
+
+    def get_password(self):
+        return self.__password
+
+
+users = shelve.open('user')
+
+
+def create_user(username, password):
+    id = str(uuid.uuid4())
+    user = User(id)
+    user.set_username(username)
+    user.set_password(password)
+    users[id] = user
+
+def get_user(username, password):
+    klist = list(users.keys())
+    for key in klist:
+        user = users[key]
+        print(user.get_username(), username, user.get_password(), password)
+        if user.get_username() == username and user.get_password() == password:
+            return user
+    return None
+
+def update_user(id, user):
+    users[id] = user
+    return users[id]
+
+def clear_user():
+    klist = list(users.keys())
+    for key in klist:
+        del users[key]
+
+def add_user(user):
+    users[user.get_id()] = user
+
+def init_db():
+    clear_user()
+    clear_blog()
+    for i in range(5):
+        create_user('user'+str(i), 'pass'+str(i))
+        create_blog('user'+str(i), 'title'+str(i), 'body'+str(i))
+
+
+
