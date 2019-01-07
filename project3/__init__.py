76413cb7c35355a1e2b9549e660420dfddd408d0
test <181530J@mymail.nyp.edu.sg>
2019-01-07 22:00:59 +0800

    Merge branch 'master' of https://github.com/Vincenttan1196/final-project
    # Conflicts:
    #	app.py

--- project3/__init__.py
+++ project3/__init__.py
@@ -5,6 +5,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
