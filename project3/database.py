906e5376bd6cd34178c2d604e33be668d9ef39b5
test <181530J@mymail.nyp.edu.sg>
2019-01-08 10:36:05 +0800

    revamp

--- project3/database.py
+++ project3/database.py
@@ -1,20 +1,6 @@
-from project3.models import User, Post
+from project3.models import User
 from project3 import db
 
-
+db.create_all()
 users = User.query.all()
-
-#other options
-User.query.first()
-User.query.filter_by(username='Corey').all()
-User.query.filter_by(username='Corey').first()
-##
-
-#other functions
-
-#user = User.query.filter_by(username='Corey').first()
-#print(user)
-
-##
-
-print(users)
+db.session.commit()
