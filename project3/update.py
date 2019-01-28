853cc331793bac2794b6e92e14221744d2c146a2
test <181530J@mymail.nyp.edu.sg>
2019-01-28 17:06:07 +0800

    e

--- project3/update.py
+++ project3/update.py
@@ -2,9 +2,12 @@ from project3.database import *
 from project3.models import User
 
 user = User.query.get(1)
-user.ratings = 3
-user.ratings = 1
-user.ratings = 5
+#user.ratings = 3
+
+print(user._ratings[0])
+print(user._ratings[2])
+print(user._ratings[4])
+
 
 db.session.commit()
 
